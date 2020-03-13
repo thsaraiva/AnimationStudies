40866f673adb472816dffee62a5e3094987f3c6f
Thiago Saraiva <Thiago.A.Saraiva@aexp.com>
2020-03-13 00:27:16 +0000

    Initial commit

---
+++ app/src/main/java/com/example/animationstudies/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.example.animationstudies
+
+import android.animation.Animator
+import android.animation.Animator.AnimatorListener
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private val textview1 by lazy { findViewById<TextView>(R.id.animatedTextView1) }
+    private val textview2 by lazy { findViewById<TextView>(R.id.animatedTextView2) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val objAnimator = animateUsingObjectAnimator()
+        animateUsingValueAnimator(objAnimator)
+
+    }
+
+    private fun animateUsingObjectAnimator(): ObjectAnimator {
+        return ObjectAnimator.ofFloat(textview2, "translationX", 500f).apply {
+            duration = 3000
+//            start()
+        }
+    }
+
+    private fun animateUsingValueAnimator(objAnimator: ObjectAnimator) {
+        val animator = ValueAnimator.ofFloat(500F).apply {
+            duration = 3000
+//            repeatCount = 2
+//            interpolator = LinearInterpolator()
+//            startDelay = 500
+            start()
+        }
+
+        animator.addListener(object : AnimatorListener {
+            override fun onAnimationRepeat(animation: Animator?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                objAnimator.start()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onAnimationStart(animation: Animator?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
+
+        animator.addUpdateListener {
+            //Notifies the occurrence of another frame of the animation.
+            println("Update")
+            println("animatedFraction = ${it.animatedFraction}")
+            println("animatedValue = ${it.animatedValue}")
+            println("currentPlayTime = ${it.currentPlayTime}")
+
+            with(it.animatedValue as Float) {
+                textview1.translationX = this
+                textview1.setTextColor(this.toInt())
+            }
+        }
+    }
+}
