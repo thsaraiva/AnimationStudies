7b1e5bc7e428025478a89871297bbb83649751db
Thiago Saraiva <Thiago.A.Saraiva@aexp.com>
2020-03-15 15:22:56 +0000

    Enabling viewgroup animations that are not related to adding and removing views

--- app/src/main/java/com/example/animationstudies/AnimateLayoutChangesExampleActivity.kt
+++ app/src/main/java/com/example/animationstudies/AnimateLayoutChangesExampleActivity.kt
@@ -1,15 +1,20 @@
 package com.example.animationstudies
 
+import android.animation.LayoutTransition.CHANGING
 import android.os.Bundle
 import android.view.View
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Example of usage of the attribute android:animateLayoutChanges in a layout xml file to enable default animations when adding and removing views
  * to a layout. Tested on LinearLayout and ConstraintLayout and it works just fine.
+ *
+ * Use ViewGroup.setLayoutTransition( LayoutTransition ) so that changes in layout which occur because of children being added to or removed from
+ * the ViewGroup are animated according to the animations defined in that LayoutTransition parameter.
  */
 class AnimateLayoutChangesExampleActivity : AppCompatActivity() {
 
@@ -21,10 +26,19 @@ class AnimateLayoutChangesExampleActivity : AppCompatActivity() {
     private val button6 by lazy { findViewById<Button>(R.id.button6) }
     private val resetButton by lazy { findViewById<Button>(R.id.reset_button) }
     private val resetButton2 by lazy { findViewById<Button>(R.id.reset_button2) }
+    private val changeTextButton by lazy { findViewById<Button>(R.id.change_text_button) }
+    private val rightContainer by lazy { findViewById<ViewGroup>(R.id.right_container) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animate_layout_changes_example)
+
+        /**
+         * This is the important bit to make animations that are not related to adding or removing views from a layout work.
+         * Things like size changes get animated when this is set and "android:animateLayoutChanges="true"" is present in the xml layout file in the
+         * view group.
+         */
+        rightContainer.layoutTransition.enableTransitionType(CHANGING)
 
         val leftButtonList = listOf(button1, button2, button3).apply {
             forEach {
@@ -47,6 +61,14 @@ class AnimateLayoutChangesExampleActivity : AppCompatActivity() {
 
         resetButton2.setOnClickListener {
             rightButtonList.forEach { it.visibility = VISIBLE }
+        }
+
+        changeTextButton.setOnClickListener {
+            if ((Math.random() * 10).toInt() % 2 == 0)
+                button4.text = "Very Long text"
+            else
+                button4.text = "text"
+
         }
     }
 }
